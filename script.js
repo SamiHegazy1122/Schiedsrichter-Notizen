f6ff6e9778fbf3b4c669e1a8890b3c0d53cef218
S.Hegazy.inf24 <s.hegazy.inf24@stud.bbbaden.ch>
2026-05-08 11:14:49 +0200

    Script Datei notizen-liste

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 if (document.getElementById("notizen-liste")) 
 {
+    const notizen = JSON.parse(localStorage.getItem("notizen") || "[]");
+    document.getElementById("notizen-liste").innerHTML = "";
 
+    const div = document.createElement("div");
+    div.className = "notiz-card";
+    div.innerHTML = "<p>Hallo</p>";
+    document.getElementById("notizen-liste").appendChild(div);
+
+    <button data-id="12345">Löschen</button>
+    div.innerHTML = "<button data-id='42'>Löschen</button>";
+    const id = e.target.getAttribute("data-id");
 
 }
 
